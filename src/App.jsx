aead336c8c7014699f3d0bfd9e6481cdaa6ad546
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-06-28 23:49:40 +0100

    gds

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
    
-    <div className="bg-[url('./pokemonbg.png')] max-w-svh min-h-svh bg-cover bg-fixed bg-center cursor-custom ">
+    <div className="bg-poke max-w-svh min-h-svh bg-cover bg-fixed bg-center cursor-custom ">
       
        
      <Aceuille/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,30 +8,30 @@ export default {
       translate: {
         '1/2': '50%',
       },
-      backgroundImage: {
-        "poke": "url('./assets/pokemon-bg.png')",
+      colors: {
+        'modal-background': '#f8f3f8',
+        'modal-text': '#181010',
       },
-     colors: {
-      'modal-background': '#f8f3f8',
-      'modal-text': '#181010',
-    },
-    boxShadow: {
-      'modal': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
-    },
-    borderWidth: {
-      '21': '21px',
-    },
-    borderColor: {
-      'modal-border': 'transparent', 
-    },
-    cursor: {
-      custom: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABFklEQVRYR9WXURLDIAhE6/0PbSdOtUpcd1Gnpv1KGpTHBpCE1/cXq+vrMph7dGvXZTtpfW10DCA5jrH1H0Jhs5E0hnZdCR+vb5S8Nn8mQCeS9BdSalYJqMBjAGzq59xAESN7VFVUgV8AZB/dZBR7QTFDCqGquvUBVVoEtgIwpQRzmANSFHgWQKExHdIrPeuMvQNDarXe6nC/AutgV3JW+6bgqQLeV8FekRtgV+ToDKEKnACYKsfZjjkam7a0ZpYTytwmgainpC3HvwBocgKOxqRjehoR9DFKNFYtOwCGYCszobeCbl26N6yyQ6g8X/Wex/rBPsNEV6qAMaJPMynIHQCoSqS9JSMmwef51LflTgCRszU7DvAGiV6mHWfsaVUAAAAASUVORK5CYII="), auto',
-      pointer: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC"), pointer',
-    },
-    fontFamily: {
-      custom: ['"YourCustomFont"', 'sans-serif'],
-      custom_p: ['"Press Start 2P"', 'sans-serif']
-    },
+      boxShadow: {
+        'modal': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+      },
+      borderWidth: {
+        '21': '21px',
+      },
+      borderColor: {
+        'modal-border': 'transparent', 
+      },
+      cursor: {
+        custom: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABFklEQVRYR9WXURLDIAhE6/0PbSdOtUpcd1Gnpv1KGpTHBpCE1/cXq+vrMph7dGvXZTtpfW10DCA5jrH1H0Jhs5E0hnZdCR+vb5S8Nn8mQCeS9BdSalYJqMBjAGzq59xAESN7VFVUgV8AZB/dZBR7QTFDCqGquvUBVVoEtgIwpQRzmANSFHgWQKExHdIrPeuMvQNDarXe6nC/AutgV3JW+6bgqQLeV8FekRtgV+ToDKEKnACYKsfZjjkam7a0ZpYTytwmgainpC3HvwBocgKOxqRjehoR9DFKNFYtOwCGYCszobeCbl26N6yyQ6g8X/Wex/rBPsNEV6qAMaJPMynIHQCoSqS9JSMmwef51LflTgCRszU7DvAGiV6mHWfsaVUAAAAASUVORK5CYII="), auto',
+        pointer: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC"), pointer',
+      },
+      backgroundImage: {
+        "poke": "url('./pokemonbg.png')",
+      },
+      fontFamily: {
+        custom: ['"YourCustomFont"', 'sans-serif'],
+        custom_p: ['"Press Start 2P"', 'sans-serif']
+      },
     },
   },
   plugins: [
@@ -41,9 +40,8 @@ export default {
         '.translate-center': {
           transform: 'translate(50%, 50%)',
         },
-      }
-      addUtilities(newUtilities, ['responsive', 'hover'])
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
     },
   ],
-}
-
+};
