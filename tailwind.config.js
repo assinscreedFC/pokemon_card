b74fa7cfc90b77cbbc43db0bedf477fa60f62b11
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-06-28 22:11:03 +0100

    omgi

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         '1/2': '50%',
       },
      backgroundImage: {
-      "poke": "url(./assets/pokemon-bg.png)"
+      "poke": "url(\"./assets/pokemon-bg.png\")"
      },
      colors: {
       'modal-background': '#f8f3f8',
@@ -30,7 +30,7 @@ export default {
       pointer: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC"), pointer',
     },
     fontFamily: {
-      custom: ['YourCustomFont', 'sans-serif'],
+      custom: ['"YourCustomFont"', 'sans-serif'],
       custom_p: ['"Press Start 2P"', 'sans-serif']
     },
     },
